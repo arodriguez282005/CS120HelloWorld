d0ab9b50c27f2b353fc3810a1bcc1b030140c4a7
arodriguez282005 <arodriguez282005@gmail.com>
2024-02-14 11:33:01 -0800

    added lines

---
+++ Chapter 5/CH5Ex5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    //print the value of multipules of 3
+    //from 3 X 1 to 3 X 10
+    int i = 1;
+    for(i=1;i<10;i++)
+    {
+        cout << i*3 << endl;
+    }
+    cout << i << endl;
+}
