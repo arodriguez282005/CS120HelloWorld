dada94799f35b24f37733711f5d5c949b1f7f484
arodriguez282005 <arodriguez282005@gmail.com>
2024-02-21 11:15:35 -0800

    added lines

---
+++ Chapter 5/CH5Ex9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number = 7;
+    int prime = 1;
+
+    for (int i = 2; i < number; i++){
+        if (number % i == 0){
+            prime = 0;
+            break;
+        }
+    }
+    if (prime)
+        cout << "This number is prime " << number << endl;
+    else
+        cout << "This number is not prime " << number << endl;
+   
+}
